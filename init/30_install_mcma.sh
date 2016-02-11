e43f7c3fbc97e2e86dab282dc05e729724bc8f43
sparklyballs <sparklyballs@gmail.com>
2016-02-11 10:54:42 +0000

    should help with issue #6

--- init/30_install_mcma.sh
+++ init/30_install_mcma.sh
@@ -10,7 +10,7 @@ if [ ! -x /minecraft/MCMA2_Linux_x86_64 ];
 	"
 	cp -v /tmp/MCMA2_Linux_x86_64 /minecraft/MCMA2_Linux_x86_64
 	chmod +x /minecraft/MCMA2_Linux_x86_64
-	cd /minecraft
+	cd /minecraft || exit
 	./MCMA2_Linux_x86_64 -setpass password -nonotice -configonly
 fi
 
